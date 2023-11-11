8fa0a40a62ef3ed4488d1f86426abb53abe8cee5
Kazuki Kuda <e235704@ie.u-ryukyu.ac.jp>
2023-11-11 12:59:01 +0900

    after

--- app/src/main/java/jp/ac/uryukyu/ie/e235704/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235704/Hero.java
@@ -9,10 +9,38 @@ package jp.ac.uryukyu.ie.e235704;
  * Created by tnal on 2016/11/13.
  */
 public class Hero {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
+        // getterメソッド
+    public boolean getDead() {
+        return dead;
+    }
+    public int getHitPoint(){
+        return hitPoint;
+    }
+    public int getAttack(){
+        return attack;
+    }
+    public String getName(){
+        return name;
+    }
+      
+        // setterメソッド
+    public void setDead(boolean dead) {
+        this.dead = dead;
+    }
+    public void setHitPoint(int hitPoint){
+        this.hitPoint = hitPoint;
+    }
+    public void setAttack(int attack){
+        this.attack = attack;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+      
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -35,7 +63,7 @@ public class Hero {
      */
     public void attack(Enemy e){
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.name, damage);
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
         e.wounded(damage);
     }
 
